2f81291c46c98f09f72dc9e616016442dbe63f6e
Sv443 <contact@sv443.net>
2024-11-27 19:48:58 +0100

    fix: ok now it should shut up

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,6 +2,7 @@
 const config = {
   preset: "ts-jest",
   testEnvironment: "node",
+  modulePathIgnorePatterns: ["<rootDir>/out/"],
   testPathIgnorePatterns: ["node_modules/", "out/"],
   testMatch: ["**/test/**/*.spec.ts"],
 };
