ffd2d394c1cd51b26a1135f2376e792ba31bf06f
Sv443 <contact@sv443.net>
2024-11-28 22:37:51 +0100

    fix: tests baseUrl

--- test/constants.ts
+++ test/constants.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-export const baseUrl = `http://127.0.0.1:${process.env.HTTP_PORT}`;
+export const baseUrl = `http://127.0.0.1:${process.env.HTTP_PORT}/v2`;
 
 const authToken = process.env.AUTH_TOKENS?.split(",")[0];
 
