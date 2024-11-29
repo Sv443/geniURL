452f10cde32391b5238de375aa2fea1e5c7654df
Sv443 <contact@sv443.net>
2024-11-29 21:17:42 +0100

    fix: remove duplicate baseUrl

--- test/album.spec.ts
+++ test/album.spec.ts
@@ -1,11 +1,10 @@
-import { baseUrl } from "./constants";
 import { checkAlbumProps, checkArtistProps, sendReq } from "./hooks";
 
 describe("Album routes", () => {
   //#region /album/:id
 
   it("Album details yields correct props", async () => {
-    const { status, res } = await sendReq(`${baseUrl}/album/7105950`);
+    const { status, res } = await sendReq("/album/7105950");
     const body = await res.json();
 
     expect(status).toBe(200);
@@ -21,7 +20,7 @@ describe("Album routes", () => {
   //#region inv /album/:id
 
   it("Invalid song ID yields error", async () => {
-    const { res, status } = await sendReq(`${baseUrl}/album/0`);
+    const { res, status } = await sendReq("/album/0");
     const body = await res.json();
 
     expect(status).toBe(400);
@@ -34,7 +33,7 @@ describe("Album routes", () => {
   //#region inv /album
 
   it("Album path without ID yields error", async () => {
-    const { res, status } = await sendReq(`${baseUrl}/album`);
+    const { res, status } = await sendReq("/album");
     const body = await res.json();
 
     expect(status).toBe(400);
