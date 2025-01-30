94841ccaeedede90cfe321e3b3c0436afa099703
Sv443 <contact@sv443.net>
2025-01-30 23:34:49 +0100

    fix: test stuff

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,7 +4,20 @@ const config = {
   testEnvironment: "node",
   modulePathIgnorePatterns: ["<rootDir>/out/"],
   testPathIgnorePatterns: ["node_modules/", "out/"],
-  testMatch: ["**/test/**/*.spec.ts"],
+  testMatch: ["**/test/**/*.spec.(mjs|mts|js|ts)"],
+  transform: {
+    "^.+\\.(c|m)?ts$": ["ts-jest", {
+      tsconfig: {
+        module: "NodeNext",
+        moduleResolution: "NodeNext",
+        target: "ES2018",
+        allowJs: true,
+        allowImportingTsExtensions: true,
+        allowSyntheticDefaultImports: true,
+        importHelpers: true,
+      }
+    }],
+  }
 };
 
 export default config;
